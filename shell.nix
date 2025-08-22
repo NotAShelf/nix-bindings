c319b2e3056f469a6ebf90e7d2c56326b240b6ac
NotAShelf <raf@notashelf.dev>
2025-08-22 15:37:47 +0300

    initial commit
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I6a6a69640bcaee05d43239c0060e6e625ff959c5

---
+++ shell.nix
@@ -0,0 +1,28 @@
+{pkgs ? import <nixpkgs> {}}:
+with pkgs;
+  mkShell {
+    packages = [
+      cargo
+      rustc
+
+      rust-analyzer-unwrapped
+      (rustfmt.override {asNightly = true;})
+      clippy
+      nix-output-monitor
+      taplo
+      yaml-language-server
+      lldb
+    ];
+
+    nativeBuildInputs = [
+      nix.dev
+      pkg-config
+      glibc.dev
+      gcc
+    ];
+
+    env = {
+      RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
+      LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+    };
+  }
