e2367480f1671b6b048f2bca9f4246b5108c1476
NotAShelf <raf@notashelf.dev>
2025-08-23 09:01:06 +0300

    examples: add version example
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I6a6a6964a26c1321222e6fdf9db850a0f4c76847

---
+++ examples/version.rs
@@ -0,0 +1,15 @@
+use nix_bindings::nix_version_get;
+use std::ffi::CStr;
+
+fn main() {
+    unsafe {
+        let version_ptr = nix_version_get();
+        if version_ptr.is_null() {
+            eprintln!("Failed to get Nix version (null pointer)");
+            std::process::exit(1);
+        }
+
+        let version = CStr::from_ptr(version_ptr).to_string_lossy();
+        println!("Nix library version: {version}");
+    }
+}
