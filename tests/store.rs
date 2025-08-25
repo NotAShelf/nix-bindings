2b7937a4d3df6fbb83ade94466583724fc79cb9d
NotAShelf <raf@notashelf.dev>
2025-08-25 11:10:20 +0300

    tests: format with new rustfmt config
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I6a6a6964d622196b8185e43c67aa6007567065cc

--- tests/store.rs
+++ tests/store.rs
@@ -1,104 +1,105 @@
 #![cfg(test)]
 
-use serial_test::serial;
-use std::ffi::CString;
-use std::ptr;
+use std::{ffi::CString, ptr};
 
 use nix_bindings::*;
+use serial_test::serial;
 
 #[test]
 #[serial]
 fn libstore_init_and_open_free() {
-    unsafe {
-        let ctx = nix_c_context_create();
-        assert!(!ctx.is_null());
-        let err = nix_libstore_init(ctx);
-        assert_eq!(err, nix_err_NIX_OK);
+  unsafe {
+    let ctx = nix_c_context_create();
+    assert!(!ctx.is_null());
+    let err = nix_libstore_init(ctx);
+    assert_eq!(err, nix_err_NIX_OK);
 
-        // Open the default store (NULL URI, NULL params)
-        let store = nix_store_open(ctx, ptr::null(), ptr::null_mut());
-        assert!(!store.is_null());
+    // Open the default store (NULL URI, NULL params)
+    let store = nix_store_open(ctx, ptr::null(), ptr::null_mut());
+    assert!(!store.is_null());
 
-        // Free the store and context
-        nix_store_free(store);
-        nix_c_context_free(ctx);
-    }
+    // Free the store and context
+    nix_store_free(store);
+    nix_c_context_free(ctx);
+  }
 }
 
 #[test]
 #[serial]
 fn parse_and_clone_free_store_path() {
-    unsafe {
-        let ctx = nix_c_context_create();
-        assert!(!ctx.is_null());
-        let err = nix_libstore_init(ctx);
-        assert_eq!(err, nix_err_NIX_OK);
+  unsafe {
+    let ctx = nix_c_context_create();
+    assert!(!ctx.is_null());
+    let err = nix_libstore_init(ctx);
+    assert_eq!(err, nix_err_NIX_OK);
 
-        let store = nix_store_open(ctx, ptr::null(), ptr::null_mut());
-        assert!(!store.is_null());
+    let store = nix_store_open(ctx, ptr::null(), ptr::null_mut());
+    assert!(!store.is_null());
 
-        // Parse a store path (I'm using a dummy path, will likely be invalid but should not segfault)
-        // XXX: store_path may be null if path is invalid, but should not crash
-        let path_str = CString::new("/nix/store/dummy-path").unwrap();
-        let store_path = nix_store_parse_path(ctx, store, path_str.as_ptr());
+    // Parse a store path (I'm using a dummy path, will likely be invalid but
+    // should not segfault) XXX: store_path may be null if path is invalid,
+    // but should not crash
+    let path_str = CString::new("/nix/store/dummy-path").unwrap();
+    let store_path = nix_store_parse_path(ctx, store, path_str.as_ptr());
 
-        if !store_path.is_null() {
-            // Clone and free
-            let cloned = nix_store_path_clone(store_path);
-            assert!(!cloned.is_null());
-            nix_store_path_free(cloned);
-            nix_store_path_free(store_path);
-        }
-
-        nix_store_free(store);
-        nix_c_context_free(ctx);
+    if !store_path.is_null() {
+      // Clone and free
+      let cloned = nix_store_path_clone(store_path);
+      assert!(!cloned.is_null());
+      nix_store_path_free(cloned);
+      nix_store_path_free(store_path);
     }
+
+    nix_store_free(store);
+    nix_c_context_free(ctx);
+  }
 }
 
 #[test]
 #[serial]
 fn store_get_uri_and_storedir() {
-    unsafe extern "C" fn string_callback(
-        start: *const ::std::os::raw::c_char,
-        n: ::std::os::raw::c_uint,
-        user_data: *mut ::std::os::raw::c_void,
-    ) {
-        let s = unsafe { std::slice::from_raw_parts(start.cast::<u8>(), n as usize) };
-        let s = std::str::from_utf8(s).unwrap();
-        let out = user_data.cast::<Option<String>>();
-        unsafe { *out = Some(s.to_string()) };
-    }
+  unsafe extern "C" fn string_callback(
+    start: *const ::std::os::raw::c_char,
+    n: ::std::os::raw::c_uint,
+    user_data: *mut ::std::os::raw::c_void,
+  ) {
+    let s =
+      unsafe { std::slice::from_raw_parts(start.cast::<u8>(), n as usize) };
+    let s = std::str::from_utf8(s).unwrap();
+    let out = user_data.cast::<Option<String>>();
+    unsafe { *out = Some(s.to_string()) };
+  }
 
-    unsafe {
-        let ctx = nix_c_context_create();
-        assert!(!ctx.is_null());
-        let err = nix_libstore_init(ctx);
-        assert_eq!(err, nix_err_NIX_OK);
+  unsafe {
+    let ctx = nix_c_context_create();
+    assert!(!ctx.is_null());
+    let err = nix_libstore_init(ctx);
+    assert_eq!(err, nix_err_NIX_OK);
 
-        let store = nix_store_open(ctx, ptr::null(), ptr::null_mut());
-        assert!(!store.is_null());
+    let store = nix_store_open(ctx, ptr::null(), ptr::null_mut());
+    assert!(!store.is_null());
 
-        let mut uri: Option<String> = None;
-        let res = nix_store_get_uri(
-            ctx,
-            store,
-            Some(string_callback),
-            (&raw mut uri).cast(),
-        );
-        assert_eq!(res, nix_err_NIX_OK);
-        assert!(uri.is_some());
+    let mut uri: Option<String> = None;
+    let res = nix_store_get_uri(
+      ctx,
+      store,
+      Some(string_callback),
+      (&raw mut uri).cast(),
+    );
+    assert_eq!(res, nix_err_NIX_OK);
+    assert!(uri.is_some());
 
-        let mut storedir: Option<String> = None;
-        let res = nix_store_get_storedir(
-            ctx,
-            store,
-            Some(string_callback),
-            (&raw mut storedir).cast(),
-        );
-        assert_eq!(res, nix_err_NIX_OK);
-        assert!(storedir.is_some());
+    let mut storedir: Option<String> = None;
+    let res = nix_store_get_storedir(
+      ctx,
+      store,
+      Some(string_callback),
+      (&raw mut storedir).cast(),
+    );
+    assert_eq!(res, nix_err_NIX_OK);
+    assert!(storedir.is_some());
 
-        nix_store_free(store);
-        nix_c_context_free(ctx);
-    }
+    nix_store_free(store);
+    nix_c_context_free(ctx);
+  }
 }
