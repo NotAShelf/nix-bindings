60fb1eb626011e82a9ae1365f0f99bc6cb1ed8ac
NotAShelf <raf@notashelf.dev>
2025-09-19 14:10:20 +0300

    meta: suppress nixdoc warnings
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I6a6a6964598b0d9ba15648535ed888dd526273d1

--- nix-bindings-sys/lib.rs
+++ nix-bindings-sys/lib.rs
@@ -1,11 +1,16 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(rustdoc::bare_urls)]
+#![allow(rustdoc::invalid_html_tags)]
+
+//! # nix-bindings-sys
+//!
 //! Raw, unsafe FFI bindings to the Nix C API.
 //!
-//! # Safety
+//! ## Safety
+//!
 //! These bindings are generated automatically and map directly to the C API.
 //! They are unsafe to use directly. Prefer using the high-level safe API in the
 //! parent crate unless you know what you're doing.
-
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
