fea1c12a1cdde964cbd6ef3003e1feac19bb6a96
NotAShelf <raf@notashelf.dev>
2025-10-24 15:13:36 +0300

    nix: add Lix 2.93 to devshell
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: Ibb4f552154e4c1b3a62961f69ccbd5466a6a6964

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
 {pkgs ? import <nixpkgs> {}}: let
   nixForBindings = pkgs.nixVersions.nix_2_28;
+  lixForBindings = pkgs.lixPackageSets.lix_2_93;
 in
   pkgs.mkShell {
     name = "nix-bindings";
@@ -18,6 +19,7 @@ in
 
     nativeBuildInputs = with pkgs; [
       nixForBindings.dev
+      lixForBindings.lix.dev
       pkg-config
       glibc.dev
       #gcc
