68854fd339ad45d85676d36b5547da23f8b17491
NotAShelf <raf@notashelf.dev>
2025-12-20 08:51:35 +0300

    flake: document motivation for tracking stable branch
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I8e7dafb62268ee38d6661c74a677c5de6a6a6964

--- flake.nix
+++ flake.nix
@@ -1,5 +1,11 @@
 {
+  # We generally want to target the stable branch of NixOS, because it allows
+  # us to infer a "minimum stable release" to support. Namely, it allows for
+  # having a reference point, so we know what is ready for public use and what
+  # is not. Security and critical performance improvements are always included
+  # so it makes a good starting point.
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+
   outputs = {nixpkgs, ...}: let
     systems = ["x86_64-linux" "aarch64-linux"];
     forEachSystem = nixpkgs.lib.genAttrs systems;
@@ -8,9 +14,7 @@
     devShells = forEachSystem (system: let
       pkgs = pkgsForEach.${system};
     in {
-      default = pkgsForEach.${system}.callPackage ./shell.nix {
-        inherit pkgs;
-      };
+      default = pkgsForEach.${system}.callPackage ./shell.nix {inherit pkgs;};
     });
   };
 }
