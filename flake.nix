f017642210c2aaee06451bec7f378041a49288ff
NotAShelf <raf@notashelf.dev>
2026-09-13 18:48:33 +0300

    flake: switch to zstd tarball for Nixpkgs; bump inputs
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I15297008912b2867fa35186adb7f142d6a6a6964

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+  inputs.nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
 
   outputs = {nixpkgs, ...}: let
     systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
